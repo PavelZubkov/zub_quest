61dce6638d655587d9edebf3c973afba1be0d5fc
PavelZubkov <PavelZubkov@users.noreply.github.com>
2023-03-15 07:09:40 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.js
+++ web.js
@@ -2665,6 +2665,9 @@ var $;
 var $;
 (function ($) {
     class $mol_book2 extends $mol_scroll {
+        menu_title() {
+            return "";
+        }
         sub() {
             return this.pages();
         }
@@ -2717,6 +2720,9 @@ var $;
                     }
                 }).reverse().filter(Boolean).join(' | ');
             }
+            menu_title() {
+                return this.pages()[0]?.title() || this.title();
+            }
             sub() {
                 const next = [...this.pages(), this.Placeholder()];
                 const prev = $mol_mem_cached(() => this.sub()) ?? [];
@@ -2759,7 +2765,7 @@ var $;
 //hyoo/hyoo.ts
 ;
 "use strict";
-let $hyoo_sync_revision = "318df19";
+let $hyoo_sync_revision = "c1ce41a";
 //hyoo/sync/-meta.tree/revision.meta.tree.ts
 ;
 "use strict";
@@ -4620,7 +4626,7 @@ var $;
         land_init(land) {
             this.db_land_init(land);
             if (!land.grabbed())
-                this.$.$mol_wait_timeout(1000);
+                this.$.$mol_wait_timeout(1);
         }
         land(id) {
             return this.world().land_sync(id);
