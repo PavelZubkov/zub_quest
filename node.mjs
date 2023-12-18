96e82b3b73d9ee6e58dba58fa425d7728f9a260b
PavelZubkov <PavelZubkov@users.noreply.github.com>
2023-12-18 07:09:09 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.mjs
+++ node.mjs
@@ -9483,7 +9483,8 @@ var $;
                 title: this.hint_safe(),
                 target: this.target(),
                 download: this.file_name(),
-                mol_link_current: this.current()
+                mol_link_current: this.current(),
+                rel: this.relation()
             };
         }
         sub() {
@@ -9517,6 +9518,9 @@ var $;
         }
         current() {
             return false;
+        }
+        relation() {
+            return "";
         }
         event_click(event) {
             if (event !== undefined)
--- web.js
+++ web.js
@@ -9183,7 +9183,8 @@ var $;
                 title: this.hint_safe(),
                 target: this.target(),
                 download: this.file_name(),
-                mol_link_current: this.current()
+                mol_link_current: this.current(),
+                rel: this.relation()
             };
         }
         sub() {
@@ -9217,6 +9218,9 @@ var $;
         }
         current() {
             return false;
+        }
+        relation() {
+            return "";
         }
         event_click(event) {
             if (event !== undefined)
