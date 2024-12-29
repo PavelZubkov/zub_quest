2f9946407c6f3785a4367865b7c5221bb5be25b4
PavelZubkov <PavelZubkov@users.noreply.github.com>
2024-12-29 07:10:03 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.js
+++ web.js
@@ -9573,7 +9573,7 @@ var $;
             Tools: {
                 flex: {
                     basis: 'auto',
-                    grow: 1000,
+                    grow: 0,
                     shrink: 1,
                 },
                 display: 'flex',
