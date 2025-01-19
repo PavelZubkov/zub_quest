a153cae7af0d06908d79849880c65728e738a517
PavelZubkov <PavelZubkov@users.noreply.github.com>
2025-01-19 07:10:16 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -7320,6 +7320,7 @@ var $;
         static go(next) {
             this.href(this.link(next));
         }
+        static commit() { }
         constructor(prefix = '') {
             super();
             this.prefix = prefix;
